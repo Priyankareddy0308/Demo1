642061f5558b1a57bf7d0e181c51117254b1182a
Priyankareddy0308 <68346594+Priyankareddy0308@users.noreply.github.com>
2020-12-02 20:52:04 +0530

    Update app_dash.py

--- src/app_dash.py
+++ src/app_dash.py
@@ -68,7 +68,7 @@ DATA_PATH = os.path.join(HOME_PATH, 'data')
 MODELS_PATH = os.path.join(HOME_PATH, 'model')
 ASSETS_PATH = os.path.join(HOME_PATH, 'assets')
 
-DP_train = pd.read_csv('../data/Customer-Value-Analysis.csv')
+DP_train = pd.read_csv('../data/Disease Prediction Training.csv')
 sk_best = load(os.path.join(MODELS_PATH, 'best.joblib'))
 
 #full_pipeline = load(os.path.join(MODELS_PATH, 'transformer.joblib'))
